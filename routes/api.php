56d5dd2081182f86aebd395124f10cd38f0cdd14
leyeleye22 <aboubacarleye34@gmail.com>
2023-12-07 13:09:35 +0000

    h

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\MairieController;
-use App\Http\Controllers\Api\ProjetController;
 use App\Http\Controllers\Api\RegionController;
 
 /*
@@ -20,15 +19,29 @@ use App\Http\Controllers\Api\RegionController;
 */
 
 //inscrire un nouveau user
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('ajout/projet', [ProjetController::class, 'store']);
-});
 //inscrire un nouveau mairie
 Route::post('/registerMairie',[MairieController::class,'registerMairie']);
 Route::post('/loginMairie',[MairieController::class,'loginMairie']);
+
+
+//gestion des regions
+
+//Recuperer la liste des posts 
+
+Route::get('regions',[RegionController::class,'index']);
+
+// Ajout d'une region |POST|PUT|PATCH
+Route::post('regions/create',[RegionController::class,'store']);
+
+
+// Modification d'une region 
+Route::put('regions/edit/{region}',[RegionController::class,'update']);
+
+// Suppression de la region
+// Route::delete('regions/{region}',[RegionController::class,'delete']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
